bce36679e1d1c1cbc94f2ed841fe71d1b3acd53c
Prahlad77044 <prolad1122@gmail.com>
2024-02-09 21:23:30 +0545

    new commit

--- lib/presentation/maps/map.dart
+++ lib/presentation/maps/map.dart
@@ -13,21 +13,22 @@ class DonorsNearby extends StatefulWidget {
 class _DonorsNearbyState extends State<DonorsNearby> {
   Completer<GoogleMapController> _controller = Completer();
   Future<Position> getUserCurrentLocation() async {
-    await Geolocator.requestPermission().then((value){
-    }).onError((error, stackTrace) async {
+    await Geolocator.requestPermission()
+        .then((value) {})
+        .onError((error, stackTrace) async {
       await Geolocator.requestPermission();
-      print("ERROR"+error.toString());
+      print("ERROR" + error.toString());
     });
     return await Geolocator.getCurrentPosition();
   }
+
   final List<Marker> _markers = <Marker>[
     Marker(
         markerId: MarkerId('1'),
         position: LatLng(20.42796133580664, 75.885749655962),
         infoWindow: InfoWindow(
           title: 'My Position',
-        )
-    ),
+        )),
   ];
 
   @override
@@ -37,60 +38,54 @@ class _DonorsNearbyState extends State<DonorsNearby> {
         title: Text('Donors Nearby'),
         backgroundColor: Colors.red[800],
       ),
-      body: Stack(
-        children: [Container(
+      body: Stack(children: [
+        Container(
           child: GoogleMap(
               initialCameraPosition: CameraPosition(
                 zoom: 17.4746,
-                target: LatLng(
-                    27.712020, 85.300140),
+                target: LatLng(27.712020, 85.300140),
               ),
               markers: Set<Marker>.of(_markers),
-
-
               myLocationEnabled: true,
               mapType: MapType.normal,
               onMapCreated: (GoogleMapController controller) {
                 _controller.complete(controller);
               }),
-
         ),
         Padding(
-          padding: const EdgeInsets.fromLTRB(340.0,580,5,20),
-          child:  FloatingActionButton(
-            onPressed: () async{
+          padding: const EdgeInsets.fromLTRB(340.0, 580, 5, 20),
+          child: FloatingActionButton(
+            onPressed: () async {
               getUserCurrentLocation().then((value) async {
-                print(value.latitude.toString() +" "+value.longitude.toString());
+                print(value.latitude.toString() +
+                    " " +
+                    value.longitude.toString());
 
                 // marker added for current users location
-                _markers.add(
-                    Marker(
-                      markerId: MarkerId("2"),
-                      position: LatLng(value.latitude, value.longitude),
-                      infoWindow: InfoWindow(
-                        title: 'My Current Location',
-                      ),
-                    )
-                );
+                _markers.add(Marker(
+                  markerId: MarkerId("2"),
+                  position: LatLng(value.latitude, value.longitude),
+                  infoWindow: InfoWindow(
+                    title: 'My Current Location',
+                  ),
+                ));
 
                 // specified current users location
                 CameraPosition cameraPosition = new CameraPosition(
                   target: LatLng(value.latitude, value.longitude),
                   zoom: 17,
-              git version  );
+                );
 
                 final GoogleMapController controller = await _controller.future;
-                controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
-                setState(() {
-                });
+                controller.animateCamera(
+                    CameraUpdate.newCameraPosition(cameraPosition));
+                setState(() {});
               });
             },
             child: Icon(Icons.my_location),
           ),
-
-        )]
-      ),
-      
+        )
+      ]),
     );
   }
 }
